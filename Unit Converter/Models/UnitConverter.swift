6d3ff364896042b36f724b172c46233db080c351
Maggie Vu <maggievu91@gmail.com>
2019-01-22 21:23:05 -0800

    first commit

---
+++ Unit Converter/Models/UnitConverter.swift
@@ -0,0 +1,15 @@
+//
+//  UnitConverter.swift
+//  Unit Converter
+//
+//  Created by Maggie on 2019-01-21.
+//  Copyright © 2019 Maggie VU. All rights reserved.
+//
+
+import Foundation
+
+class UnitConverter {
+    func degreeF(degreeC: Int) -> Int {
+        return (Int(Float(degreeC) * 1.8 + 32.0))
+    }
+}
---
+++ Unit Converter/ViewModels/TemperatureRange.swift
@@ -0,0 +1,9 @@
+//
+//  TemperatureRange.swift
+//  Unit Converter
+//
+//  Created by Maggie on 2019-01-21.
+//  Copyright © 2019 Maggie VU. All rights reserved.
+//
+
+import Foundation
