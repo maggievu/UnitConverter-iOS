d9b26514eb4f04764761c4c8620365167d0b5e9a
Maggie Vu <maggievu91@gmail.com>
2019-01-28 02:16:56 -0800

    Assignment 2 for review

---
+++ Unit Converter/ViewModels/Responder.swift
@@ -0,0 +1,18 @@
+//
+//  UISegmentedControl.swift
+//  Unit Converter
+//
+//  Created by Maggie on 2019-01-28.
+//  Copyright © 2019 Maggie VU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//class Responder: NSObject {
+//    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+//        UIView.animate(withDuration: 0.3) {
+//            UIView.frame.origin.x = (UISegmentedControl.frame.width / CGFloat(UISegmentedControl.numberOfSegments)) * CGFloat(UISegmentedControl.selectedSegmentIndex)
+//        }
+//    }
+//}
--- Unit Converter/ViewModels/TemperatureRange.swift
+++ Unit Converter/ViewModels/TemperatureRange.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2019 Maggie VU. All rights reserved.
 //
 
+import UIKit
 import Foundation
+
+class TemperatureRange: NSObject, UIPickerViewDataSource {
+    
+    let values = (-100...100).map { $0 }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return values.count
+    }
+}
